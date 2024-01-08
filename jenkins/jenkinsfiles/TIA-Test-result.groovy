d3f4ca2e948dde11c58aca41011d075bc48d252f
ahmadabed1 <Ahmad.aabed@sealights.io>
2024-01-08 10:03:54 +0200

    add TIA-Test-result

--- jenkins/jenkinsfiles/TIA-Test-result.groovy
+++ jenkins/jenkinsfiles/TIA-Test-result.groovy
@@ -21,11 +21,10 @@ pipeline {
     string(name: 'BRANCH', defaultValue: 'BTQ-TIA', description: 'Branch to clone')
     string(name: 'INTEGRAION_BRANCH', defaultValue: 'ahmad-branch', description: 'integration branch Branche')
     string(name: 'APP_NAME', defaultValue: 'ahmad-BTQ', description: 'app name')
+    string(name: 'LAB_ID', defaultValue: '', description: 'lab_id')
     string(name: 'RUN_DATA', defaultValue: 'full run', description: 'RUN_NUMBER loop number')
     string(name: 'LAB_UNDER_TEST',defaultValue: 'https://dev-integration.dev.sealights.co/api',description: 'The lab you want to test\nE.g. "https://dev-keren-gw.dev.sealights.co/api"')
     string(name: 'SEALIGHTS_ENV_NAME', defaultValue: 'dev-integration')
-
-
 
 
   }
@@ -47,40 +46,22 @@ pipeline {
     }
 
 
-    stage('download NodeJs agent and scanning Mocha tests supported') {
+    stage('download NodeJs agent and scanning Mocha tests') {
       steps{
         script{
           tools.set_npm_registries()
           sh """
-            export RUN_DATA="${params.RUN_DATA}"
+            echo {"\\"server"\\":  "\\"${params.LAB_UNDER_TEST}"\\", "\\"env"\\":  "\\"${params.SEALIGHTS_ENV_NAME}"\\"} >server.json
             export APP_NAME="${params.APP_NAME}"
             export BRANCH_NAME="${params.INTEGRAION_BRANCH}"
-            echo {"\\"server"\\":  "\\"${params.LAB_UNDER_TEST}"\\", "\\"env"\\":  "\\"${params.SEALIGHTS_ENV_NAME}"\\"} >server.json
+            export LAB_ID="${params.LAB_ID}"
+            export RUN_DATA="${params.RUN_DATA}"
             export EXTERNAL_CUSTOMER_ID="integration"
             export EXTERNAL_USER_EMAIL="integration@sealights.io"
             export EXTERNAL_USER_PASSWORD="SeaLights2019!"
             npm install
             ./node_modules/.bin/tsc
-            ./node_modules/mocha/bin/_mocha tsOutputs/BTQ/TIA-Tests/supported-TIA-spec.js --no-timeouts
-          """
-        }
-      }
-    }
-
-    stage('download NodeJs agent and scanning Mocha tests unsupported frameworks') {
-      steps{
-        script{
-          tools.set_npm_registries()
-          sh """
-            export RUN_DATA="${params.RUN_DATA}"
-            export APP_NAME="${params.APP_NAME}"
-            export BRANCH_NAME="${params.INTEGRAION_BRANCH}"
-            echo {"\\"server"\\":  "\\"${params.LAB_UNDER_TEST}"\\", "\\"env"\\":  "\\"${params.SEALIGHTS_ENV_NAME}"\\"} >server.json
-            export EXTERNAL_CUSTOMER_ID="integration"
-            export EXTERNAL_USER_EMAIL="integration@sealights.io"
-            export EXTERNAL_USER_PASSWORD="SeaLights2019!"
-            ./node_modules/.bin/tsc
-            ./node_modules/mocha/bin/_mocha tsOutputs/BTQ/TIA-Tests/unsupported-TIA-spec.js --no-timeouts
+            ./node_modules/mocha/bin/_mocha tsOutputs/BTQ/TIA-Tests/TIA-test-result-spec.js --no-timeouts
           """
         }
       }
