a0a604c2a9ca3e8f7508406fdab739b19c48f74b
AhmadSealights <ahmad.wahbi@sealights.io>
2024-01-09 09:46:28 +0200

    .

--- integration-tests/cypress/integration/api.spec.js
+++ integration-tests/cypress/integration/api.spec.js
@@ -1,6 +1,15 @@
+before(() => {
+  // makes custom commands available to all subsequent cy.origin('cypress.io)
+  // calls in this spec. put it in your support file to make them available to
+  // all specs
+  cy.origin('cypress.io', () => {
+    Cypress.require('../support/commands')
+  })
+})
+
 describe('Api Tests', () => {
   it('should return 200 for index page', () => {
-    cy.request('GET', '/').then(response => {
+    cy.apiRequest('GET', '/').then(response => {
       cy.wrap(response.status).should('equal', 200);
     });
   });
@@ -8,7 +17,7 @@ describe('Api Tests', () => {
   it('should be able to set different currencies', () => {
     const currencies = ['EUR', 'USD', 'JPY', 'CAD'];
     for (const currency of currencies) {
-      cy.request('POST', '/setCurrency', { currency_code: currency }).then(response => {
+      cy.apiRequest('POST', '/setCurrency', { currency_code: currency }).then(response => {
         cy.wrap(response.status).should('equal', 200);
       });
     }
@@ -22,20 +31,20 @@ describe('Api Tests', () => {
     ];
 
     for (const product_id of products) {
-      cy.request('GET', `/product/${product_id}`).then(response => {
+      cy.apiRequest('GET', `/product/${product_id}`).then(response => {
         cy.wrap(response.status).should('equal', 200);
       });
     }
   });
 
   it('should return 404 for a non-existent route', () => {
-    cy.request('GET', '/nonexistent-route', null, { failOnStatusCode: false }).then(response => {
+    cy.apiRequest('GET', '/nonexistent-route', null, { failOnStatusCode: false }).then(response => {
       cy.wrap(response.status).should('equal', 404);
     });
   });
 
  it('should return 200 for invalid request data', () => {
-   cy.request('POST', '/setCurrency', { invalid_key: 'invalid_value' }, { failOnStatusCode: false }).then(response => {
+   cy.apiRequest('POST', '/setCurrency', { invalid_key: 'invalid_value' }, { failOnStatusCode: false }).then(response => {
      cy.wrap(response.status).should('equal', 200);
    });
  });
@@ -46,3 +55,10 @@ describe('Api Tests', () => {
 // Run the tests automatically in Cypress:
 // npm install cypress --save-dev
 // npx cypress run --spec "cypress/integration/api.spec.js"
+
+
+
+
+
+
+
