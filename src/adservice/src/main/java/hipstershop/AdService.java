bb2f2bf715c81d35a77f7f83f47b01c14bacb784
AhmadSealights <ahmad.wahbi@sealights.io>
2024-04-25 14:57:18 +0300

    .

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -131,6 +131,7 @@
             logger.log(Level.INFO, "Custom code executed2");
             logger.log(Level.INFO, "Custom code executed3");
             logger.log(Level.INFO, "Custom code executed4");
+            logger.log(Level.INFO, "Custom code executed6");
          }
      }
  
