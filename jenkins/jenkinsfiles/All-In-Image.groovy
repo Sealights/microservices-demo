3090b8013624828f4dff8f6b265a37d0b6d9a4ad
ahmadabed1 <Ahmad.aabed@sealights.io>
2024-04-25 09:41:49 +0300

    CHANGE BT_PASSWORD

--- jenkins/jenkinsfiles/All-In-Image.groovy
+++ jenkins/jenkinsfiles/All-In-Image.groovy
@@ -36,7 +36,7 @@ pipeline {
       steps{
         script{
           dir('integration-tests'){
-            env.GT_PASSWORD =  "${script.secrets.get_secret('mgmt/github_token','us-west-2')}"
+            env.GT_PASSWORD =  secrets.get_secret('mgmt/github_token','us-west-2')
             echo "${env.GT_PASSWORD}"
 
             env.GRADLE_VERSION =(sh(returnStdout: true, script: """gh api \\
