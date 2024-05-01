b7b6f8a2cbb36c8e7c6668160a9382dad7408db2
ahmadabed1 <Ahmad.aabed@sealights.io>
2024-05-01 19:44:16 +0300

    fix

--- src/cartservice/src/cartstore/RedisCartStore.cs
+++ src/cartservice/src/cartstore/RedisCartStore.cs
@@ -1,18 +1,29 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Caching.Distributed;
 using Google.Protobuf;
-using Hipstershop;
 
 namespace cartservice.cartstore
 {
     public class RedisCartStore : ICartStore
     {
         private readonly IDistributedCache _cache;
-        private const double DiscountThreshold = 100.0;
-        private const double DiscountRate = 0.10; // 10% discount
 
         public RedisCartStore(IDistributedCache cache)
         {
@@ -22,23 +33,24 @@ namespace cartservice.cartstore
         public async Task AddItemAsync(string userId, string productId, int quantity)
         {
             Console.WriteLine($"AddItemAsync called with userId={userId}, productId={productId}, quantity={quantity}");
+
             try
             {
-                Cart cart;
+                Hipstershop.Cart cart;
                 var value = await _cache.GetAsync(userId);
                 if (value == null)
                 {
-                    cart = new Cart();
+                    cart = new Hipstershop.Cart();
                     cart.UserId = userId;
-                    cart.Items.Add(new CartItem { ProductId = productId, Quantity = quantity });
+                    cart.Items.Add(new Hipstershop.CartItem { ProductId = productId, Quantity = quantity });
                 }
                 else
                 {
-                    cart = Cart.Parser.ParseFrom(value);
+                    cart = Hipstershop.Cart.Parser.ParseFrom(value);
                     var existingItem = cart.Items.SingleOrDefault(i => i.ProductId == productId);
                     if (existingItem == null)
                     {
-                        cart.Items.Add(new CartItem { ProductId = productId, Quantity = quantity });
+                        cart.Items.Add(new Hipstershop.CartItem { ProductId = productId, Quantity = quantity });
                     }
                     else
                     {
@@ -49,7 +61,7 @@ namespace cartservice.cartstore
             }
             catch (Exception ex)
             {
-                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex.Message}"));
+                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex}"));
             }
         }
 
@@ -59,54 +71,35 @@ namespace cartservice.cartstore
 
             try
             {
-                var cart = new Cart();
+                var cart = new Hipstershop.Cart();
                 await _cache.SetAsync(userId, cart.ToByteArray());
             }
             catch (Exception ex)
             {
-                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex.Message}"));
+                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex}"));
             }
         }
 
-        public async Task<Cart> GetCartAsync(string userId)
+        public async Task<Hipstershop.Cart> GetCartAsync(string userId)
         {
             Console.WriteLine($"GetCartAsync called with userId={userId}");
 
             try
             {
+                // Access the cart from the cache
                 var value = await _cache.GetAsync(userId);
+
                 if (value != null)
                 {
-                    return Cart.Parser.ParseFrom(value);
+                    return Hipstershop.Cart.Parser.ParseFrom(value);
                 }
-                // We decided to return an empty cart in cases when user wasn't in the cache before
-                return new Cart();
+
+                // We decided to return empty cart in cases when user wasn't in the cache before
+                return new Hipstershop.Cart();
             }
             catch (Exception ex)
             {
-                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex.Message}"));
-            }
-        }
-
-        public async Task<double> GetCartTotalWithDiscountAsync(string userId)
-        {
-            try
-            {
-                var cart = await GetCartAsync(userId);
-                double total = cart.Items.Sum(item => item.Price * item.Quantity);
-
-                if (total > DiscountThreshold)
-                {
-                    double discount = total * DiscountRate;
-                    total -= discount;
-                    Console.WriteLine($"Discount of {discount:C} applied. New total: {total:C}");
-                }
-
-                return total;
-            }
-            catch (Exception ex)
-            {
-                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Error calculating cart total with discount: {ex.Message}"));
+                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex}"));
             }
         }
 
@@ -114,8 +107,7 @@ namespace cartservice.cartstore
         {
             try
             {
-                // Simply checks if the cache is accessible
-                return _cache != null;
+                return true;
             }
             catch (Exception)
             {
