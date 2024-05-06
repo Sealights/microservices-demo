9fa665e6378770b631a60f0973a65faff2c27baa
ahmadabed1 <Ahmad.aabed@sealights.io>
2024-05-06 17:52:47 +0300

    fix

--- jenkins/jenkinsfiles/All-In-Image.groovy
+++ jenkins/jenkinsfiles/All-In-Image.groovy
@@ -103,6 +103,7 @@ pipeline {
                     "sealightsJvmParams": {}
                     }' > slmaventests.json
             echo "Adding Sealights to Tests Project POM file..."
+            sed -i 's/^mavenPassword=.*/mavenPassword='${env.GH_TOKEN}'/' gradle.properties
             java -jar /sealights/sl-build-scanner.jar -pom -configfile slmaventests.json -workspacepath .
             #mvn dependency:get -Dartifact=io.sealights.on-premise.agents.plugin:sealights-maven-plugin:4.0.103  -gs ./settings-github.xml
             mvn clean package
