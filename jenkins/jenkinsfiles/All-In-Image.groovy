e74d0895c22b27ef0687b9e5c0360846a26c2060
ahmadabed1 <Ahmad.aabed@sealights.io>
2024-05-12 16:47:23 +0300

    fix

--- jenkins/jenkinsfiles/All-In-Image.groovy
+++ jenkins/jenkinsfiles/All-In-Image.groovy
@@ -64,7 +64,7 @@ pipeline {
                   ./node_modules/.bin/slnodejs start --tokenfile ./sltoken.txt --labid ${params.SL_LABID} --teststage "CucumberJS-Tests"
                   node_modules/.bin/cucumber-js ./features --require \$SL_PACKAGE --require 'features/**/*.@(js|cjs|mjs)'
                   ./node_modules/.bin/slnodejs end --tokenfile ./sltoken.txt --labid ${params.SL_LABID}
-                  sleep ${env.wait_time}
+                  sleep 10
                   """
           }
         }
