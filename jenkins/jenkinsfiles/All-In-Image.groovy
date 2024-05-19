e2dad5a619c82062c94cfd5992bcd1cc65b0c8e4
ahmadabed1 <Ahmad.aabed@sealights.io>
2024-05-19 17:13:54 +0300

    fix

--- jenkins/jenkinsfiles/All-In-Image.groovy
+++ jenkins/jenkinsfiles/All-In-Image.groovy
@@ -71,6 +71,7 @@ pipeline {
           if (params.Run_all_tests == true || params.NUnit == true) {
             sh """
                 echo 'N-Unit framework starting ..... '
+                  mkdir -p ./sealights/agent
                   DOTNET_LATEST_VERSION=gh release view --repo sealights/SL.OnPremise.Agents.DotNet --json tagName --jq '.tagName'
                   gh release download \$DOTNET_LATEST_VERSION --repo sealights/SL.OnPremise.Agents.DotNet -D ./sealights/agent
                   RUN unzip ./sealights/agent/sealights-dotnet-agent-linux-self-contained.zip -d /app/sealights/agent
